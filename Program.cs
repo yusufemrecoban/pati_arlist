90d7b6199f87251f45448c6005897ce25205a7a7
Yusuf Emre ÇOBAN <202013709005@ogr.balikesir.edu.tr>
2023-08-02 22:55:51 +0300

    ArrayList Yapısı

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace Hell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //System.Collections namespace
+            ArrayList liste=new ArrayList();
+            liste.Add("alp");
+            liste.Add(18);
+            liste.Add(true);
+            liste.Add('A');
+
+            //içerisinde verilenlere erişim 
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste){
+                Console.WriteLine(item);
+            }
+            
+            //AddRange
+            string[] renkler={"kırmızı","sarı","yeşil"};
+            List<int> sayilar=new List<int>();
+            liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            //Sort
+            //liste.Sort();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            //BinarySearch
+            Console.WriteLine(liste.BinarySearch("alp"));
+            //Reverse
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            //Clear
+            liste.Clear();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }*/
+        }
+    }
+}
